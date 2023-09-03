48ebf3e67f6a5832e0603a38f28bdfd07ee98c86
Alon Lavy <alon.lavy2@gmail.com>
2023-09-03 16:48:55 +0300

    added many images by mapping an array

--- src/Components/HomeArea/Home/Home.tsx
+++ src/Components/HomeArea/Home/Home.tsx
@@ -1,9 +1,14 @@
-import "./Home.css";
-import fruitsAndVeggies from "../../../Assets/food.png";
-import drinks from "../../../Assets/drinks.jpg";
 import { useState } from "react";
 import Switch from "react-switch";
-import { deserialize } from "v8";
+import drinks from "../../../Assets/drinks.jpg";
+import fruitsAndVeggies from "../../../Assets/food.png";
+
+import img1 from "../../../Assets/product1.jpg";
+import img2 from "../../../Assets/product2.jpg";
+import img3 from "../../../Assets/product3.jpg";
+import img4 from "../../../Assets/product4.jpg";
+import img5 from "../../../Assets/product5.jpg";
+import "./Home.css";
 
 function Home(): JSX.Element {
 	const [counter, setCounter] = useState(0);
@@ -12,6 +17,14 @@ function Home(): JSX.Element {
 		{ id: 2, name: "Marshmallow", price: 2 },
 		{ id: 3, name: "Cake", price: 4 },
 		{ id: 4, name: "Chocolate", price: 1 },
+	];
+
+	const images = [
+		{ id: 1, image: img1, name: "hello1" },
+		{ id: 2, image: img2, name: "hello2" },
+		{ id: 3, image: img3, name: "hello3" },
+		{ id: 4, image: img4, name: "hello4" },
+		{ id: 5, image: img5, name: "hello5" },
 	];
 
 	const today = new Date().getDay();
@@ -39,6 +52,16 @@ function Home(): JSX.Element {
 					</div>
 				);
 			})}
+			<div>
+				{images.map((image) => {
+					return (
+						<div key={image.id * 20} className="flexColumn">
+							<img src={image.image} key={image.id}></img>
+							<span key={image.id * 10}>{image.name}</span>
+						</div>
+					);
+				})}
+			</div>
 			<p>{today === 5 || today === 6 ? "Weekend Sale" : "Weekday Sale"}</p>
 		</div>
 	);
