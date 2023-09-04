23e45a0625585b4fd31b9118cc7d9214f197ae60
Alon Lavy <alon.lavy2@gmail.com>
2023-09-04 11:26:08 +0300

    useEffect

--- src/Components/AboutArea/About/About.tsx
+++ src/Components/AboutArea/About/About.tsx
@@ -1,5 +1,5 @@
 import Snackbar from "@mui/material/Snackbar";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import WhoAreWe from "../WhoAreWe/WhoAreWe";
 import "./About.css";
@@ -7,34 +7,46 @@ import "./About.css";
 function About(): JSX.Element {
 	const [isOpen, setIsOpen] = useState<boolean>(false);
 	const [time, setTime] = useState<string>(null as unknown as string);
-	const navigator = useNavigate();    
+	const [clock, setClock] = useState<string>("");
+	const navigator = useNavigate();
 
 	const [topProduct, setTopProduct] = useState("---");
 	const displayTopProduct = (): void => {
 		setTopProduct("Ice coffee");
 	};
 
+	useEffect(() => {
+		const interval = setInterval(() => {
+			console.log(new Date().toLocaleTimeString());
+			setClock(new Date().toLocaleTimeString());
+		}, 1000);
+		return () => {
+			clearInterval(interval);
+			console.log("closed");
+		};
+	}, []);
+
 	return (
 		<div className="About">
-			<p>
-				<WhoAreWe />
+			<WhoAreWe />
 
-				<button onClick={() => setIsOpen(true)}>Show Current Date</button>
-				<Snackbar
-					open={isOpen}
-					autoHideDuration={2500}
-					message={new Date().toLocaleDateString()}
-					onClose={() => setIsOpen(false)}
-					anchorOrigin={{ vertical: "top", horizontal: "center" }}
-				/>
-				<button onClick={() => navigator("/home")}>Back Home</button>
-				<button onClick={displayTopProduct}>Display top product</button>
-				<span>{topProduct}</span>
+			<button onClick={() => setIsOpen(true)}>Show Current Date</button>
+			<Snackbar
+				open={isOpen}
+				autoHideDuration={2500}
+				message={new Date().toLocaleDateString()}
+				onClose={() => setIsOpen(false)}
+				anchorOrigin={{ vertical: "top", horizontal: "center" }}
+			/>
+			<button onClick={() => navigator("/home")}>Back Home</button>
+			<button onClick={displayTopProduct}>Display top product</button>
+			<span>{topProduct}</span>
 
-				<button onClick={() => setTime(new Date().toLocaleTimeString())}>
-					{time === null ? "Show Current Time" : time}
-				</button>
-			</p>
+			<button onClick={() => setTime(new Date().toLocaleTimeString())}>
+				{time === null ? "Show Current Time" : time}
+			</button>
+
+			<span>{clock}</span>
 		</div>
 	);
 }
