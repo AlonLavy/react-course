b56acb32eca3320242e3c43366750c7e89bee896
Alon Lavy <alon.lavy2@gmail.com>
2023-09-04 13:07:51 +0300

    using state to manage pseudo form

--- src/Components/HomeArea/Search/Search.tsx
+++ src/Components/HomeArea/Search/Search.tsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
 import "./Search.css";
 
 function Search(): JSX.Element {
+	const [searchInput, setSearchInput] = useState<string>();
 	return (
 		<div className="Search">
 			<label>Search Something</label>
-			<input type="search" />
-            <button>🔍</button>
-            <span></span>
+			<input
+				type="search"
+				onChange={(event) => {
+					setSearchInput(event.target.value);
+				}}
+			/>
+			<button
+				onClick={() => {
+					alert(`searching ${searchInput}`);
+				}}
+			>
+				🔍
+			</button>
+			<span>{searchInput}</span>
 		</div>
 	);
 }
